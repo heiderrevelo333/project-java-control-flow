78d805b5379ef4d41d10d7b030e98febb99fddeb
Esteban Revelo <estebanadso21@gmail.com>
2025-05-13 19:01:30 -0500

    Desafio FizzBuzz

--- src/FizzBuzzBoom.java
+++ src/FizzBuzzBoom.java
@@ -3,33 +3,29 @@ import java.util.Scanner;
 public class FizzBuzzBoom {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Solicitar al usuario el rango
-        System.out.print("Ingrese el número inicial: ");
+
+        // Solicitar al usuario los límites del rango
+        System.out.print("Introduce el número inicial: ");
         int inicio = scanner.nextInt();
-        System.out.print("Ingrese el número final: ");
+        System.out.print("Introduce el número final: ");
         int fin = scanner.nextInt();
-        
+
         for (int i = inicio; i <= fin; i++) {
-            boolean esMultiploDe3 = (i % 3 == 0);
-            boolean esMultiploDe5 = (i % 5 == 0);
-            boolean esMultiploDe7 = (i % 7 == 0);
-            
-            if (esMultiploDe3 && esMultiploDe5 && esMultiploDe7) {
+            if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0) {
                 System.out.println("FizzBuzzBoom");
-            } else if (esMultiploDe3 && esMultiploDe5) {
+            } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
-            } else if (esMultiploDe3) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            } else if (esMultiploDe5) {
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            } else if (esMultiploDe7) {
+            } else if (i % 7 == 0) {
                 System.out.println("Boom");
             } else {
                 System.out.println(i);
             }
         }
-
+        
         scanner.close();
     }
 }
